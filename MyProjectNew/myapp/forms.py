da8f28bf2ffc63c6ae04fa8d89ced61f781eb638
carlos matheus <carlos.matheus.10@hotmail.com>
2018-12-04 11:26:57 -0300

    Alterações de layout de formulario e ações editar e detalhar

--- MyProjectNew/myapp/forms.py
+++ MyProjectNew/myapp/forms.py
@@ -1,16 +1,22 @@
 from django.forms import ModelForm, Textarea
 from django.forms import TextInput
+from django.forms import Field
 from myapp.models import Post
 
 class PostForm(ModelForm):
 
     class Meta:
         model = Post
-        fields = ['title', 'text', 'email']
+        fields = ('title', 'text', 'email')
         widgets = {
             'title': TextInput(attrs={'type': 'text', 'class': 'form-control'}),
             'text': Textarea(attrs={'class' : 'form-control', 'aria-label' : 'Com textarea'}),
-            'email': TextInput(attrs={'type': 'text', 'class': 'form-control', 'placeholder' : 'Usuário', 'aria-label' : 'Usuário', 'aria-describedby' : 'basic-addon1'}),
+            'email': TextInput(attrs={'type': 'text', 'class': 'form-control', 'placeholder' : 'example@gmail.com', 'aria-label' : 'Usuário', 'aria-describedby' : 'basic-addon1'}),
+        }
+        labels = {
+            'title': ('Título da Postagem'),
+            'text': ('Texto da Postagem'),
+            'email': ('Login de Usuário (Email)'),
         }
 
     def clean(self):
--- MyProjectNew/myapp/views.py
+++ MyProjectNew/myapp/views.py
@@ -30,32 +30,12 @@ def add_post(request):
         return render(request, "add_post.html", {'form' : form})
 
 def edit_post(request, post_id):
-    '''
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            model_instance = form.save(commit=False)
-            model_instance.date_published = datetime.datetime.now()
-            model_instance.save()
-            return redirect('list_posts')
-        else:
-            return render(request, "add_post.html", {'form': form})
-    else:
-        form = PostForm()
-        return render(request, "add_post.html", {'form': form})
-'''
-
 
     post_a_editar = Post.objects.get(id = post_id)
     form = PostForm(request.POST, instance=post_a_editar)
-    if form.is_valid():
-        model_instance = form.save(commit=False)
-        model_instance.date_published = datetime.datetime.now()
-        model_instance.save()
-        return redirect('list_posts')
-    else:
-        return render(request, "edit_post.html",
-                  {'form' : form})
+
+    return render(request, "edit_post.html",
+                  {'form' : form, 'post_a_editar': post_a_editar})
 
 def detail_post(request, post_id):
     post = Post.objects.get(id = post_id)
