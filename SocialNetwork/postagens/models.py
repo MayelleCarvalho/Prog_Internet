7795259127af63e862ec3c21025c7e226fe17c28
Mayelle Carvalho <carvalhomayelle@gmail.com>
2019-01-17 16:06:00 -0300

    Create Project social network

---
+++ SocialNetwork/postagens/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+# Profile: id, name, email, address { street, suite, city, zipcode } ;
+# • Comment: id, name, email, body, postId;
+# • Post: id, title, body, userId.
+
