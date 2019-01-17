9e5ffb8d1dda85ec122ad5250c5e6e19807a6636
Mayelle Carvalho <carvalhomayelle@gmail.com>
2019-01-17 15:31:01 -0300

    Conclusão de games api por fazer

--- gamesapi_por_fazer/games/models.py
+++ gamesapi_por_fazer/games/models.py
@@ -15,7 +15,7 @@ class Game(models.Model):
     name = models.CharField(max_length=200)
     release_date = models.DateTimeField()
     played = models.BooleanField(default=False)
-    game_category=models.ForeignKey(GameCategory,related_name='games',on_delete=models.CASCADE)
+    game_category = models.ForeignKey(GameCategory,related_name='games',on_delete=models.CASCADE)
     class Meta:
         ordering = ('name',)
 
--- gamesapi_por_fazer/games/urls.py
+++ gamesapi_por_fazer/games/urls.py
@@ -12,12 +12,12 @@ from games import views
 urlpatterns = [
     path('', views.ApiRoot.as_view(), name=views.ApiRoot.name),
     path('scores/', views.ScoreList.as_view(), name=views.ScoreList.name),
-    path('scores/<int:id>/', views.ScoreDetail.as_view(), name=views.ScoreDetail.name),
+    path('scores/<int:pk>/', views.ScoreDetail.as_view(), name=views.ScoreDetail.name),
     path('games/', views.GameList.as_view(), name=views.GameList.name),
-    path('games/<int:id>/', views.GameDetail.as_view(), name=views.GameDetail.name),
+    path('games/<int:pk>/', views.GameDetail.as_view(), name=views.GameDetail.name),
     path('players/', views.PlayerList.as_view(), name=views.PlayerList.name),
-    path('players/<int:id>/', views.PlayerDetail.as_view(), name=views.PlayerDetail.name),
+    path('players/<int:pk>/', views.PlayerDetail.as_view(), name=views.PlayerDetail.name),
     path('game-categories/', views.GameCategoryList.as_view(), name=views.GameCategoryList.name),
-    path('game-categories/<int:id>/', views.GameCategoryDetail.as_view(), name=views.GameCategoryDetail.name),
+    path('game-categories/<int:pk>/', views.GameCategoryDetail.as_view(), name=views.GameCategoryDetail.name),
 ]
 
