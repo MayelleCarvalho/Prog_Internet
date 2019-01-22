aad9ea7480d00a0bb2625bbb08973747643d2730
Mayelle Carvalho <carvalhomayelle@gmail.com>
2019-01-22 00:35:39 -0200

    primeira questão: sql para json

--- SocialNetwork/postagens/models.py
+++ SocialNetwork/postagens/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 
-# Create your models here.
-# Profile: id, name, email, address { street, suite, city, zipcode } ;
-# • Comment: id, name, email, body, postId;
-# • Post: id, title, body, userId.
+class Users(models.Model):
+
+    ADDRESS_CHOICE = (
+        ('STREET', 'Street'),
+        ('SUITE', 'Suite'),
+        ('CITY','City'),
+        ('ZIPCODE', 'Zipcode'),
+    )
+    name = models.CharField(max_length=100, blank=False, default='')
+    email = models.CharField(max_length=100)
+    address = models.CharField(max_length=7, choices=ADDRESS_CHOICE, default='STREET')
+
+
+class Comments(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    body = models.CharField(max_length=500)
+    postId = models.ForeignKey(Users, on_delete=models.CASCADE)
 
