a667b98718234c4dd700ec53f84b879c02a6c9ef
Mayelle Carvalho <carvalhomayelle@gmail.com>
2019-02-04 10:52:45 -0300

    Atualização de repositorio com os projetos da disciplina

---
+++ MySocialNetwork/myapp/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+# Create your models here.
+
+class Endereco(models.Model):
+  rua = models.CharField(max_length=100)
+  bairro = models.CharField(max_length=100)
+  numero = models.IntegerField()
+  complemento = models.CharField(max_length=100)
+  cep = models.CharField(max_length=10)
+  cidade = models.CharField(max_length=100)
+
+
+class Empresa (models.Model):
+  nome = models.CharField(max_length=100)
+  cnpj = models.CharField(max_length=18)
+  sloga = models.CharField(max_length=200)
+
+
+class Usuario (models.Model):
+  nome_usuario = models.CharField(max_length=20)
+  website = models.CharField(max_length=50)
+  nome = models.CharField(max_length=20)
+  empresa = models.CharField(max_length=200)
+  phone = models.CharField(max_length=200)
+  endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE, related_name='enderecos')
+  email = models.EmailField()
+
+
+class Post(models.Model):
+  body = models.CharField(max_length=200)
+  autor = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='usuarios')
+  title = models.CharField(max_length=100)
+
+
+class Comments(models.Model):
+  body = models.CharField(max_length=500)
+  email = models.CharField(max_length=500)
+  autor = models.ForeignKey(Post,on_delete=models.CASCADE, related_name='autores')
+  name = models.CharField(max_length=50)
+
+
